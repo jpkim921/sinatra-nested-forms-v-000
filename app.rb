1e3c492d92ae62e9f186c3f61eb6232c64611315
Jayon Kim <jpkim921@gmail.com>
2018-05-26 16:21:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
-require './environment'
+require_relative './environment'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      # binding.pry
+      # @pirate = Pirate.new(pirate[:name], pirate[:weight], pirate[:height])
+      # @first_ship = Ship.new(pirate[ships][][:name], pirate[ships][][:type], pirate[ships][][:booty])
+      # @second_ship = Ship.new(pirate[ships][][:name], pirate[ships][][:type], pirate[ships][][:booty])
+
+      erb :'pirates/show'
+    end
 
   end
 end
